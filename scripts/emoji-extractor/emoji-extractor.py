c902d0cb3b0957fe1c91ca6ca89a7feb5e2d7b04
ariasuni <perso@hack-libre.org>
2017-05-13 14:47:24 +0200

    massively factorize emoji-extractor and improve stdout a bit

--- scripts/emoji-extractor/emoji-extractor.py
+++ scripts/emoji-extractor/emoji-extractor.py
@@ -31,16 +31,32 @@ font_xml = io.StringIO()
 TTFont(args.input).saveXML(font_xml)
 ttx = ElementTree.fromstring(font_xml.getvalue())
 
+def mass_rename(ligature_set_or_emoji):
+    cmap = ttx.find('cmap').find('cmap_format_12')
+    code = ''
+    for map_element in cmap:
+        if map_element.attrib['name'].lower() == ligature_set_or_emoji:
+            replace_s = '' if len(map_element.attrib['code']) > 4 else '00'
+            code = map_element.attrib['code'].replace('0x', replace_s)
+            rename('emoji_{}.png'.format(ligature_set_or_emoji),
+                   'emoji_u{}.png'.format(code))
+
+def rename(old_name, new_name):
+    print('Renaming {} to {}'.format(old_name, new_name))
+    try:
+        (path / old_name).rename(path / new_name)
+    except:
+        print('!! Cannot rename {}'.format(old_name))
+
 for element in ttx.find('CBDT').find('strikedata'):
     data = element.find('rawimagedata').text.split()
-    name = element.attrib['name'].lower()
-    name = name.replace('uni', 'u')
+    name = element.attrib['name'].lower().replace('uni', 'u')
     image_path = path / 'emoji_{}.png'.format(name)
     print('Extracting {}'.format(image_path.name))
     emoji = open(image_path, "wb")
     for char in data:
-            hexChar = binascii.unhexlify(char)
-            emoji.write(hexChar)
+        hexChar = binascii.unhexlify(char)
+        emoji.write(hexChar)
     emoji.close
 
 for ligature_set_xml in ttx.find('GSUB')\
@@ -54,45 +70,17 @@ for ligature_set_xml in ttx.find('GSUB')\
                 .replace(',', '_')\
                 .replace('uni', 'u')
             glyph = ligature_xml.attrib['glyph']
-            old_name = 'emoji_{}.png'.format(glyph)
-            new_name = 'emoji_{}_{}.png'.format(ligature_set, component)
-            print('Renaming {} to {}'.format(old_name, new_name))
-            try:
-                (path / old_name).rename(path / new_name)
-            except:
-                print('!! Cannot rename {}'.format(old_name))
+            rename('emoji_{}.png'.format(glyph),
+                   'emoji_{}_{}.png'.format(ligature_set, component))
     else:
-        cmap = ttx.find('cmap').find('cmap_format_12')
-        code = ''
-        for map_element in cmap:
-            if map_element.attrib['name'] == ligature_set:
-                replace_s = '' if len(map_element.attrib['code']) > 4 else '00'
-                code = map_element.attrib['code'].replace('0x', replace_s)
-                old_name = 'emoji_{}.png'.format(ligature_set)
-                try:
-                    (path / old_name).rename(path / 'emoji_u{}.png'.format(code))
-                except:
-                    print('!! Cannot rename {}'.format(old_name))
-        else:
-            if code == '':
-                print('Ignoring {}...'.format(ligature_set))
+        mass_rename(ligature_set)
 
 emojis = path.glob('*.png')
 for emoji in emojis:
     if not emoji.name.startswith('emoji_u'):
         emoji = emoji.with_name(emoji.name.replace('emoji_', '').replace('.png', ''))
         print('Fixing {}...'.format(emoji.name))
-        cmap = ttx.find('cmap').find('cmap_format_12')
-        code = ''
-        for map_element in cmap:
-            if map_element.attrib['name'].lower() == emoji:
-                replace_s = '' if len(map_element.attrib['code']) > 4 else '00'
-                code = map_element.attrib['code'].replace('0x', replace_s)
-                old_name = path / 'emoji_{}.png'.format(emoji)
-                try:
-                    (path / old_name).rename(path / 'emoji_u{}.png'.format(code))
-                except:
-                    print('!! Cannot fix _{}'.format(old_name))
+        mass_rename(emoji)
 
 # Some flags aren't correctly sorted
 trans_table = {
@@ -109,4 +97,7 @@ trans_table = {
 }
 
 for old, new in trans_table.items():
-    (path / 'emoji_u{}.png'.format(old)).rename(path / 'emoji_u{}.png'.format(new))
+    old_path = path / 'emoji_u{}.png'.format(old)
+    new_path = path / 'emoji_u{}.png'.format(new)
+    print('Renaming incorrect {} to {}'.format(old_path.name, new_path.name))
+    old_path.rename(new_path)
